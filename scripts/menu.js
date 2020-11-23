971d2addef54b2a4d0a6558fb3d5abb860cbcac1
Octanne <contact@octanne.eu>
2020-11-23 17:01:01 +0100

    update style

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,11 +2,13 @@ var menuOpen = false;
 
 function openMenu() {
     document.getElementById("menu").style.width = "100%";
+    //document.getElementById("menuBtn").innerHTML = "&#x2715;";
     menuOpen = true;
 }
 
 function closeMenu() {
     document.getElementById("menu").style.width = "0%";
+    //document.getElementById("menuBtn").innerHTML = "&#9776;";
     menuOpen = false;
 }
 
