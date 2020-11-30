e6a033b9dafbb73bb71caae30a668c0292f83c3e
Octanne <contact@octanne.eu>
2020-11-30 14:12:45 +0100

    fix navbar responsive & add chang logo on menu when open

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,13 +2,15 @@ var menuOpen = false;
 
 function openMenu() {
     document.getElementById("menu").style.width = "100%";
-    //document.getElementById("menuBtn").innerHTML = "&#x2715;";
+    document.getElementById("menuBtn").innerHTML = "&#x2715;";
+	document.getElementById("menuBtn").style.marginRight = "0.08em";
     menuOpen = true;
 }
 
 function closeMenu() {
     document.getElementById("menu").style.width = "0%";
-    //document.getElementById("menuBtn").innerHTML = "&#9776;";
+    document.getElementById("menuBtn").innerHTML = "&#9776;";
+	document.getElementById("menuBtn").style.marginRight = "auto";
     menuOpen = false;
 }
 
